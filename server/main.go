910e6e6f3a83d339b3fc8c1b93a3fb04fcd362d2
Lanre Adelowo <yo@lanre.wtf>
2019-02-04 21:39:37 +0100

    Add cors support

--- server/main.go
+++ server/main.go
@@ -90,6 +90,14 @@ const (
 
 func createFeedTitle(client *pusher.Client, f *feed) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == http.MethodOptions {
+			return
+		}
+
 		writer := json.NewEncoder(w)
 
 		type respose struct {
